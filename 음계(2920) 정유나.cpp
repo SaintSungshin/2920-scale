1edd88b7c2f42522cc3d67eb5c670b07a2e8dc6e
una0106 <44970801+una0106@users.noreply.github.com>
2018-11-21 05:48:08 +0900

    Add files via upload

---
+++ 음계(2920) 정유나.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int list[8] = { 0 };
+	for (int i = 0; i < 8; i++) {
+		list[i] = i + 1;
+	}
+	
+	int answer[8] = { 0 };
+	cout << "Enter the 8 numbers : ";
+	for (int i = 0; i < 8; i++) {
+		cin >> answer[i];
+	}
+
+
+	int j = 0;
+	for (int i = 0; i < 8; i++) {
+			if (list[i] == answer[j])
+				j++;
+	}
+	if (j == 8) 
+		cout << "ascending " << endl;
+
+	int a = 0;
+		for (int i = 0,k = 7; i < 8; i++,k--) {
+					if (list[i] == answer[k])
+						a++;
+		}
+
+	if (a == 8)
+		cout << "descending" << endl;
+	else          //이 부분이 문제 인듯....
+		cout << "mixed" << endl;
+
+	system("pause");
+	return 0;
+}
